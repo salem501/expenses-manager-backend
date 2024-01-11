32175077574e529cb14df60824fad42a6bf6f275
salem <salemslimi0@gmail.com>
2024-01-11 21:51:38 +0100

    change security config

--- src/main/java/com/BudgetWise/templates/config/JwtService.java
+++ src/main/java/com/BudgetWise/templates/config/JwtService.java
@@ -38,7 +38,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
--- src/main/java/com/BudgetWise/templates/config/SecurityConfig.java
+++ src/main/java/com/BudgetWise/templates/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/**")
+                .requestMatchers("auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
